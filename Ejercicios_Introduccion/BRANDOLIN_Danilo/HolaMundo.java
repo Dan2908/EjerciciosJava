80d2f76e66a9ab2fea40416cda6430c721103e14
Dan2908 <63173102+Dan2908@users.noreply.github.com>
2021-03-20 11:02:16 -0300

    Se agregan capturas del funcionamiento

--- Ejercicios_Introduccion/BRANDOLIN_Danilo/HolaMundo.java
+++ Ejercicios_Introduccion/BRANDOLIN_Danilo/HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo{
 	public static void main(String[] args){
+		//println(): Salida con salto de linea
 		System.out.println("Hola Mundo");
 	}
 }
--- Ejercicios_Introduccion/BRANDOLIN_Danilo/HolaMundo2.java
+++ Ejercicios_Introduccion/BRANDOLIN_Danilo/HolaMundo2.java
@@ -1,10 +1,14 @@
-import java.util.Scanner; //
+import java.util.Scanner; // importamos paquete que contiene la clase Scanner
 
 public class HolaMundo2{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in); 			//objeto de la clase Scanner
-		System.out.print("Ingresa tu nombre: "); 		//print salida sin salto de linea
-		String nombre = sc.nextLine();					//Leer entrada (String)
-		System.out.printf("Tu nombre es %s", nombre); 	//printf salida formateada
+			//Creamos objeto de la clase Scanner
+		Scanner sc = new Scanner(System.in);
+			//print(): salida sin salto de linea
+		System.out.print("Ingresa tu nombre: ");
+			//Leer entrada (String)
+		String nombre = sc.nextLine();
+			//printf(); salida formateada
+		System.out.printf("Tu nombre es %s", nombre);
 	}
 }
