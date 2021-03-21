7f6efe5ca3e9329b43f352e30e936eb91cd3f622
Dan2908 <63173102+Dan2908@users.noreply.github.com>
2021-03-21 20:25:38 -0300

    Update Simple4.java

--- Ejercicios_Simples/BRANDOLIN_Danilo/Simple4.java
+++ Ejercicios_Simples/BRANDOLIN_Danilo/Simple4.java
@@ -8,6 +8,6 @@ public class Simple4{
 	public static void main(String[] args){
 		System.out.printf("2*6-30/50 >= 15 = %B%n", 2*6-30/50 >= 15);
 		System.out.printf("15*60/20<20*60 = %B%n", 15*60/20<20*60);
-		System.out.printf("14*3-10==(14*6-10)/2 AND 6/2*2 != 6*2/2 = %B", 14*3-10==(14*6-10)/2 || 6/2*2 != 6*2/2);
+		System.out.printf("14*3-10==(14*6-10)/2 AND 6/2*2 != 6*2/2 = %B", 14*3-10==(14*6-10)/2 && 6/2*2 != 6*2/2);
 	}
 }
